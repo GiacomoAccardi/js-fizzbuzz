969d0729476740632782c5ef18ae2ad19d1b004a
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-27 16:33:57 +0200

    [UPD] script now writes Buzz if the number is multiple of 5

--- script/script.js
+++ script/script.js
@@ -3,7 +3,9 @@
 for (let i = 1; i <= 100; i++) {
     console.log(i);
     //Facciamo in modo che per i numeri che sono multipli di 3 lo script sampi "fizz"
-    if(i % 3 ==0){
+    if(i % 3 == 0){
         console.log(`Fizz!`)
+    } else if (i % 5 == 0){
+        console.log(`Buzz!`)
     }
 }
