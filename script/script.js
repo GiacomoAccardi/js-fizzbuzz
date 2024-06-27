370500067245a87d8ef23641cc75072a648266db
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-27 16:53:45 +0200

    [UPD] ESERCIZIO BASE COMPLETATO

--- script/script.js
+++ script/script.js
@@ -2,13 +2,13 @@
 
 for (let i = 1; i <= 100; i++) {
     
-    if(i % 3 == 0 && i % 5 == 0){ //Facciamo in modo che per i numeri che sono multipli di 3 e 5 lo script stampi "FizzBuzz"
+    if(i % 3 == 0 && i % 5 == 0){ //Facciamo in modo che per i numeri che sono multipli di 3 e 5 lo script stampi "FizzBuzz"!
         console.log(`FizzBuzz!`);
-    } else if (i % 3 == 0){ //Facciamo in modo che per i numeri che sono multipli di 3 lo script stampi "fizz"
+    } else if (i % 3 == 0){ //Facciamo in modo che per i numeri che sono multipli di 3 lo script stampi "fizz"!
         console.log(`Fizz!`);
-    } else if (i % 5 == 0){ //Facciamo in modo che per i numeri che sono multipli di 5 lo script stampi "Buzz"
+    } else if (i % 5 == 0){ //Facciamo in modo che per i numeri che sono multipli di 5 lo script stampi "Buzz"!
         console.log(`Buzz!`);
     } else {
-        console.log(i); // Facciamo in modo che stampi il corrispettivo numero in tutte le altre circostanze
+        console.log(i); // Facciamo in modo che stampi il corrispettivo numero in tutte le altre circostanze!
     }
 }
